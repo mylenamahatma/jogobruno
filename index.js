ee3335757f9bed38af38230081f4ae68ac6896ce
Mylena Mahatma <104845006+mylenamahatma@users.noreply.github.com>
2023-04-22 14:44:33 -0300

    jogo processo de pftware

---
+++ index.js
@@ -0,0 +1,334 @@
+const canvas = document.querySelector('canvas')
+const c = canvas.getContext('2d')
+
+canvas.width = 1024
+canvas.height = 576
+
+c.fillRect(0, 0, canvas.width, canvas.height)
+
+const gravity = 0.7 
+
+const background = new Sprite({
+	position: {
+		x: 0,
+		y: 0
+	},
+	imageSrc: './img/background.png'
+})
+
+const shop = new Sprite({
+	position: {
+		x: 600,
+		y: 128
+	},
+	imageSrc: './img/shop.png',
+	scale: 2.75,
+	framesMax: 6
+})
+
+const player = new Fighter({
+	position: {
+		x:0,
+		y:0
+	},
+	velocity: {
+		x:0,
+		y:0
+	},
+	offset:{
+		x:0,
+		y:0
+	},
+	imageSrc: './img/samuraiMack/Idle.png',
+	framesMax:10,
+	scale: 2.5,
+	offset: {
+		x:168,
+		y:100
+	},
+	sprites: {
+		idle: {
+			imageSrc:'./img/samuraiMack/Idle.png',
+			framesMax:10
+		},
+		run: {
+			imageSrc:'./img/samuraiMack/Run.png',
+			framesMax: 8
+		},
+		jump: {
+			imageSrc:'./img/samuraiMack/Jump.png',
+			framesMax: 3
+		},
+		fall: {
+			imageSrc:'./img/samuraiMack/Fall.png',
+			framesMax: 3
+		},
+		attack1: {
+			imageSrc:'./img/samuraiMack/Attack1.png',
+			framesMax: 7
+		},
+		takeHit: {
+			imageSrc:'./img/samuraiMack/Take hit.png',
+			framesMax: 3
+		},
+		death: {
+			imageSrc:'./img/samuraiMack/Death.png',
+			framesMax: 7
+		}
+	},
+	attackBox: {
+		offset: {
+			x: 63,
+			y:50
+		},
+		width:95,
+		height:50
+	}
+
+})
+
+const enemy = new Fighter({
+	position: {
+		x: 400,
+		y:100
+	},
+	velocity: {
+		x:0,
+		y:0
+	}, 
+	color: 'blue',
+	offset:{
+		x: -50,
+		y:0
+	},
+	imageSrc: './img/kenji/Idle.png',
+	framesMax:8,
+	scale: 2.5,
+	offset: {
+		x:168,
+		y:93
+	},
+	sprites: {
+		idle: {
+			imageSrc:'./img/kenji/Idle.png',
+			framesMax: 8
+		},
+		run: {
+			imageSrc:'./img/kenji/Run.png',
+			framesMax: 8
+		},
+		jump: {
+			imageSrc:'./img/kenji/Jump.png',
+			framesMax: 2
+		},
+		fall: {
+			imageSrc:'./img/kenji/Fall.png',
+			framesMax: 2
+		},
+		attack1: {
+			imageSrc:'./img/kenji/Attack1.png',
+			framesMax: 7
+		},
+		takeHit: {
+			imageSrc:'./img/kenji/Take hit.png',
+			framesMax: 3
+		},
+		death: {
+			imageSrc:'./img/kenji/Death.png',
+			framesMax: 8
+		}
+	},
+	attackBox: {
+		offset: {
+			x: -100,
+			y:50
+		},
+		width:100,
+		height:50
+	}
+})
+console.log(player)
+
+const keys = {
+	a: {
+		pressed: false
+	},
+	d: {
+		pressed: false
+	},
+	w: {
+		pressed: false
+	},
+	ArrowRight: {
+		pressed: false
+	},
+	ArrowLeft: {
+		pressed: false
+	}
+}
+decreaseTimer()
+
+function animate() {
+	window.requestAnimationFrame(animate)
+	c.fillStyle = 'black'
+	c.fillRect(0, 0, canvas.width, canvas.height)
+	background.update()
+	shop.update()
+	c.fillStyle = 'rgba(255, 255, 255, 0.15)'
+	c.fillRect(0, 0, canvas.width, canvas.height)
+	player.update()
+	enemy.update()
+
+player.velocity.x = 0
+enemy.velocity.x = 0
+
+//movimento do jogador
+	
+	if (keys.a.pressed && player.lastKey === 'a') {
+    player.velocity.x = -5
+    player.switchSprite('run')
+  } else if (keys.d.pressed && player.lastKey === 'd') {
+    player.velocity.x = 5
+    player.switchSprite('run')
+  } else {
+    player.switchSprite('idle')
+  }
+
+//Pulando
+	 if (player.velocity.y < 0) {
+    player.switchSprite('jump')
+  } else if (player.velocity.y > 0) {
+    player.switchSprite('fall')
+  }
+
+	//movimento do inimigo
+	if (keys.ArrowLeft.pressed && enemy.lastKey === 'ArrowLeft') {
+    enemy.velocity.x = -5
+    enemy.switchSprite('run')
+  } else if (keys.ArrowRight.pressed && enemy.lastKey === 'ArrowRight') {
+    enemy.velocity.x = 5
+    enemy.switchSprite('run')
+  } else {
+    enemy.switchSprite('idle')
+  }
+
+	//Pulando
+	if (enemy.velocity.y < 0) {
+    enemy.switchSprite('jump')
+  } else if (enemy.velocity.y > 0) {
+    enemy.switchSprite('fall')
+  }
+	
+	//detector de colisão  e inimigo hit
+	if (
+		rectangularCollision({
+			rectangular1: player,
+			rectangular2: enemy
+		}) && 
+	  player.isAttacking &&
+	   player.framesCurrent === 4
+	 ){
+	 	enemy.takeHit()
+	 	player.isAttacking = false
+		
+		gsap.to('#enemyHealth', {
+			width:enemy.health + '%'
+		})
+	}
+
+	//se o jogador não acerta
+	if (player.isAttacking && player.framesCurrent === 4) {
+    player.isAttacking = false
+  }
+
+	//onde o jogador é atingido 
+
+	if (
+		rectangularCollision({
+			rectangular1: enemy,
+			rectangular2: player
+		}) && 
+	  enemy.isAttacking && 
+	   enemy.framesCurrent === 1
+	 ){
+	  player.takeHit()
+	  enemy.isAttacking = false
+		
+		gsap.to('#playerHealth', {
+			width:player.health + '%'
+		})
+	}
+		//se o inimigo não acerta
+	if(enemy.isAttacking && enemy.framesCurrent === 1) {
+		enemy.isAttacking = false
+	}
+
+
+	// Fim do Jogo pela Vida 
+	if (enemy.health <= 0 || player.health <= 0 ){
+		determineWinner({player, enemy, timerId})
+	}
+}
+
+animate()
+window.addEventListener('keydown', (event) => {
+	if (!player.dead) {
+switch (event.key) {
+	case 'd':
+		keys.d.pressed = true
+		player.lastKey = 'd'
+		break
+	case 'a':
+		keys.a.pressed = true
+		player.lastKey = 'a'
+		break
+	case 'w':
+	        player.velocity.y = -20
+        break
+      case ' ':
+        player.attack()
+        break
+    }
+  }
+
+  if (!enemy.dead) {
+    switch (event.key) {
+      case 'ArrowRight':
+        keys.ArrowRight.pressed = true
+        enemy.lastKey = 'ArrowRight'
+        break
+      case 'ArrowLeft':
+        keys.ArrowLeft.pressed = true
+        enemy.lastKey = 'ArrowLeft'
+        break
+      case 'ArrowUp':
+        enemy.velocity.y = -20
+        break
+      case 'ArrowDown':
+        enemy.attack()
+
+        break
+    }
+  }
+})
+
+
+window.addEventListener('keyup', (event) => {
+  switch (event.key) {
+    case 'd':
+      keys.d.pressed = false
+      break
+    case 'a':
+      keys.a.pressed = false
+      break
+  }
+  // enemy keys
+  switch (event.key) {
+    case 'ArrowRight':
+      keys.ArrowRight.pressed = false
+      break
+    case 'ArrowLeft':
+      keys.ArrowLeft.pressed = false
+      break
+  }
+})
